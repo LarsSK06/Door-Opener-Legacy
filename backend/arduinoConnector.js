f9e737c4181198b760ab66a060c4d27102a413f4
ApenJulius <34554076+ApenJulius@users.noreply.github.com>
2023-09-17 13:32:15 +0200

    Cooldown added to arduino with some basic return values that can be printed

--- backend/arduinoConnector.js
+++ backend/arduinoConnector.js
@@ -3,23 +3,33 @@ const { Board, Servo } = require('johnny-five')
 
 class ArduinoControl extends Board {
     constructor () {
-        super()
+        super() // Inits Board as well
         this.on('ready', this.ready.bind(this))
         this.servo = null
+        this.servoReady = false
     }
     ready() {
         this.servo = new Servo(8)
+        this.servoReady = true
         console.log('Arduino ready for use') 
     }
     openDoor() {
         if (this.servo === null) 
             return 'Servo is not ready yet'
-        
+        else if (this.servoReady === false) 
+            return 'Servo is already moving'
+        this.servoReady = false
         this.servo.max()
+        setTimeout(() => { // This is non blocking
+            this.servo.min() // All this executes after time. Code below continues before this is done
+            this.cooldown()
+        }, 1000) // 2seconds
+        return 'door function completed' // Technically returns *before* its completed
+    }
+    cooldown() { // How long between each click the arduino with allow
         setTimeout(() => {
-            this.servo.min()
-        }, 1000)
-        return 'door function completed'
+            this.servoReady = true
+        }, 2000) // 2seconds
     }
 }
 
