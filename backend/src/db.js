4dcfe560babd039c3788e1cb0bc78ea0843a494b
ApenJulius <34554076+ApenJulius@users.noreply.github.com>
2023-09-17 15:26:54 +0200

    basic hash to start with

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,7 +1,8 @@
 const Sequelize = require('sequelize')
+const bcrypt = require('bcrypt')
+const Users = require('../models/Users')
 
-
-class DataBaseManger {
+class DataBaseManager {
     constructor() {
         this.sequelize = new Sequelize({
             dialect: 'sqlite',
@@ -14,8 +15,42 @@ class DataBaseManger {
             .catch((error) => {
                 console.error('Error creating tables:', error)
             })
-
     }
+    async createAccount(userName, password) {
+        const saltRounds = 10
+        console.log(userName, password)
+        bcrypt.hash(password, saltRounds, (err, hash) => {
+            if (err) {
+                // Handle error
+                // Probably add some kind of logging system to files later
+                return 'Account NOT created!'
+            } else {
+                // Store the hashed password in database
+                Users.create({
+                    name: userName,
+                    password: hash
+                })
+                return 'Account created!'
+            }
+        })
+    }
+    async login(userName, password) {
+        bcrypt.compare(password, await Users.findOne({ where: { name:userName } }), (err, result) => {
+            if (err) {
+                // Handle error
+                // Probably add some kind of logging system to files later
+            } else if (result) {
+                // Passwords match, proceed with authentication
+                console.log('Authentication successful')
+                return 'Logged in!'
+            } else {
+                // Passwords don't match, authentication failed
+                console.log('Authentication failed')
+                return 'NOT logged in!'
+            }
+        }) 
+    }     
 }
 
-module.exports = DataBaseManger
+
+module.exports = DataBaseManager
