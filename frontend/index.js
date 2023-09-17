92b513ce154af16597d50a853ba6d2926db746d8
Lars S.K <lars.synslien.kvihaugen@gmail.com>
2023-09-17 02:48:34 +0200

    Fiksa driten

--- frontend/index.js
+++ frontend/index.js
@@ -1,37 +1,44 @@
 const ip = "127.0.0.1:4000";
 
 window.addEventListener("load", async () => {
-    document.querySelector("#container").querySelector("#submit").addEventListener("click", async () => {
-        const username = document.querySelector("#container").querySelector("#username").value;
-        const password = document.querySelector("#container").querySelector("#password").value;
-        if(username == null || username == ""){
-            log("Username field cannot be empty!");
-            return;
-        }
-        const response = await fetch(
-            `http://${ip}/open`,
-            {
-                method: "POST",
-                mode: "no-cors",
-                body: {
-                    username: "username",
-                    password: "password"
-                },
+    {
+        const container = document.querySelector("#container");
+        container.querySelector("#submit").addEventListener("click", async () => {
+            const username = document.querySelector("#container").querySelector("#username").value;
+            const password = document.querySelector("#container").querySelector("#password").value;
+            if(username == null || username == ""){
+                log("Username field cannot be empty!");
+                return;
+            }
+            const response = await fetch(`http://${ip}/s`, {
+                method: "PUT",
                 headers: {
                     "Content-Type": "application/json",
-                    "Content-Length": 1000,
-                    "Host": "127.0.0.1:4000"
-                }
+                    "Authorization": "just-for-security-idfk"
+                },
+                body: JSON.stringify({
+                    username: username,
+                    password: password
+                })
+            })
+            const data = await response.json();
+            console.log(data);
+            switch(data["abbr"]){
+                case "iah":
+                    log(response.status + ": Invalid authorization header");
+                    break;
+                case "buopm":
+                    log(response.status + ": Request body missing username or password");
+                    break;
+                case "uopi":
+                    log(response.status + ": Username or password incorrect");
+                    break;
+                case "srs":
+                    log(response.status + ": Successfully received signal");
+                    break;
             }
-        );
-        console.log(await response["abbr"]);
-        // switch(await response["abbr"]){
-        //     case "": {
-                
-        //     }
-        //     break;
-        // }
-    });
+        });
+    }
     function log(message){
         const logField = document.querySelector("#container").querySelector("#log");
         logField.innerHTML = message;
--- server.js
+++ server.js
@@ -8,95 +8,59 @@
 
 
 
-// Variables
-
-    let encryptionKey = [
-        "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
-        "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
-        "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"
-    ];
-    const encryptionChars = [
-        "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
-        "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
-        "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"
-    ];
-    const cooldown = 10;
-    let onCooldown = false;
-    let refreshEncryptionKeyTimeout;
-
-
-
 // Express app config
 
     const app = express();
     app.disable("x-powered-by");
     app.use(express.json());
     app.use(cors());
-    app.use(express.urlencoded({extended: true}));
 
 
 
 // Events
 
-    app.post("/open", (request, response) => {
-        console.log(request.body);
-        return; //
+    app.put("/s", (request, response) => {
+        if(request.headers.authorization != "just-for-security-idfk"){
+            response.status(401).send({
+                code: 401,
+                message: "Invalid authorization header",
+                abbr: "iah"
+            });
+            return;
+        }
         if(
-            !body
-            || !body["username"]
-            || !body["password"]
+            !request.body
+            || !request.body["username"]
+            || !request.body["password"]
         ){
             response.status(400).send({
                 code: 400,
-                message: "Body missing username or password",
-                abbr: "bm"
+                message: "Request body missing username or password",
+                abbr: "buopm"
             });
             return;
         }
-        if(!users[body["username"].toLowerCase()]){
-            response.status(400).send({
-                code: 400,
-                message: "User not found",
-                abbr: "unf"
+        if(!users[request.body["username"].toLowerCase()]){
+            response.status(404).send({
+                code: 404,
+                message: "Username or password incorrect",
+                abbr: "uopi"
             });
             return;
         }
-        if(!users[body["username"].toLowerCase()]["password"] != body["password"]){
-            response.status(400).send({
-                code: 401,
-                message: "Incorrect password",
-                abbr: "ip"
+        if(users[request.body["username"].toLowerCase()]["password"] != request.body["password"]){
+            response.status(404).send({
+                code: 404,
+                message: "Username or password incorrect",
+                abbr: "uopi"
             });
             return;
         }
-        if(onCooldown){
-            response.status(408).send({
-                code: 408,
-                message: "On cooldown",
-                abbr: "oc"
-            });
-            return;
-        }
-
-        // do arduino shit
-
         response.status(200).send({
             code: 200,
-            message: "Door was opened",
-            abbr: "dwo"
+            message: "Successfully received signal",
+            abbr: "srs"
         });
-        cmd.exec(`py main.py "${body["username"]}"`);
-        console.log(`\nApproved door open request:`
-            + `\n\tip:\t\t${request.ip}`
-            + `\n\thost:\t\t${request.hostname}`
-            + `\n\turl:\t\t${request.url}`
-            + `\n\tmethod:\t\t${request.method}`
-            + `\n\tuser:\t\t${body["username"]}`
-        );
-        onCooldown = true;
-        if(refreshEncryptionKeyTimeout) clearTimeout(refreshEncryptionKeyTimeout);
-        refreshEncryptionKey();
-        setTimeout(() => onCooldown = false, cooldown * 1000);
     });
 
     app.all("*", (request, response) => { // Fallback
@@ -104,10 +68,10 @@
             response.status(200).sendFile(`${__dirname}/frontend${request.url}`);
             return;
         }
-        response.status(404).send({
+        response.status(404).end(JSON.stringify({
             code: 404,
             message: "Unknown request"
-        });
+        }));
         console.log(`\nUnknown request:`
             + `\n\tip:\t\t${request.ip}`
             + `\n\thost:\t\t${request.hostname}`
@@ -119,37 +83,4 @@
     const port = 4000;
     const session = app.listen(port, "0.0.0.0", () => {
         console.log(`API active on port ${port}!`);
-        refreshEncryptionKey();
     });
-
-
-
-// Functions
-
-    function decrypt(string){
-        let result = "";
-        const split = string.split("");
-        split.forEach(char => {
-            if(encryptionKey.includes(char)){
-                result += encryptionChars[
-                    encryptionKey.indexOf(char)
-                ];
-            }
-            else result += char;
-        });
-        return result;
-    }
-    function refreshEncryptionKey(){
-        let result = [];
-        let arr = encryptionChars;
-        for(let i = 0; i < encryptionChars.length; i++){
-            const char = arr[
-                Math.floor(
-                    Math.random() * arr.length
-                )
-            ];
-            result.push(char);
-            arr.splice(arr.indexOf(char), 1);
-        }
-        return result;
-    }
